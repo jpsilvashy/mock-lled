9625bc31cdfffd5428ec6dfda684e2fa04024732
Joseph Silvashy <jpsilvashy@gmail.com>
2013-11-25 11:32:21 -0800

    puts some notes above tests

--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,5 @@
+#
+
 require 'rubygems'
 require 'eventmachine'
 require 'date'
@@ -5,7 +7,7 @@ require 'date'
 require 'rainbow'
 require 'color'
 
-class UDPHandler < EM::Connection
+class Server < EM::Connection
   def receive_data(command)
     command.chomp!
 
@@ -16,18 +18,10 @@ class UDPHandler < EM::Connection
     # Turn second byte
     color = input[1].to_f / 255.0 * 360.0
 
-    log color
-
     # Rotate 90 degrees and flip to match dial ui on device
-    color = Color::HSL.new( color - 90, 90, 50).to_rgb
+    hue = color - 90
 
-    # Get each channel and prepare for loggers output
-    red   = color.r * 255
-    green = color.g * 255
-    blue  = color.b * 255
-
-    # Log the color and the hex of the color
-    log "████████ ".color(red, green, blue) + color.html
+    log_color(hue)
 
   end
 
@@ -36,8 +30,22 @@ class UDPHandler < EM::Connection
   def log(message)
     puts "#{DateTime.now.to_s} : #{message}"
   end
+
+  def log_color(hue)
+    #
+    color = Color::HSL.new( hue, 90, 50).to_rgb
+
+    # Get each channel and prepare for loggers output
+    red   = color.r * 255
+    green = color.g * 255
+    blue  = color.b * 255
+
+    # Log the color and the hex of the color
+    log "████████ ".color(red, green, blue) + color.html
+  end
+
 end
 
 EM.run do
-  EM.open_datagram_socket('0.0.0.0', 8899, UDPHandler)
+  EM.open_datagram_socket('0.0.0.0', 8899, Server)
 end
--- spec/mock_lled_spec.rb
+++ spec/mock_lled_spec.rb
@@ -1,6 +1,33 @@
+# Tests should cover these commands, which can also be found here:
+# http://www.limitlessled.com/dev/
+
+# RGBW COLOR LED ALL OFF          0x41    65
+# RGBW COLOR LED ALL ON           0x42    66
+
+# DISCO SPEED SLOWER              0x43    67
+# DISCO SPEED FASTER              0x44    68
+
+# GROUP 1 ALL ON                  0x45    69 (SYNC/PAIR RGB+W Bulb within 2 seconds of Wall Switch Power being turned ON)
+# GROUP 1 ALL OFF                 0x46    70
+# GROUP 2 ALL ON                  0x47    71 (SYNC/PAIR RGB+W Bulb within 2 seconds of Wall Switch Power being turned ON)
+# GROUP 2 ALL OFF                 0x48    72
+# GROUP 3 ALL ON                  0x49    73 (SYNC/PAIR RGB+W Bulb within 2 seconds of Wall Switch Power being turned ON)
+# GROUP 3 ALL OFF                 0x4A    74
+# GROUP 4 ALL ON                  0x4B    75 (SYNC/PAIR RGB+W Bulb within 2 seconds of Wall Switch Power being turned ON)
+# GROUP 4 ALL OFF                 0x4C    76
+
+# DISCO MODE                      0x4D    77
+
+# SET COLOR TO WHITE (GROUP ALL)  0x42    100ms followed by:  0xC2
+# SET COLOR TO WHITE (GROUP 1)    0x45    100ms followed by:  0xC5
+# SET COLOR TO WHITE (GROUP 2)    0x47    100ms followed by:  0xC7
+# SET COLOR TO WHITE (GROUP 3)    0x49    100ms followed by:  0xC9
+# SET COLOR TO WHITE (GROUP 4)    0x4B    100ms followed by:  0xCB
+
 require 'spec_helper'
 
-describe MockLLed do
+describe Server do
+
 
 end
 
