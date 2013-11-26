9adc366b36b8d282fd414af064d3138704ce8b30
Joseph Silvashy <jpsilvashy@gmail.com>
2013-11-26 09:38:06 -0800

    cleaning up code

--- lib/limitless_led.rb
+++ lib/limitless_led.rb
@@ -1,2 +1,25 @@
 require './lib/limitless_led/bridge'
 require './lib/limitless_led/server'
+
+module LimitlessLed
+
+  def self.log(message)
+    puts "#{DateTime.now.to_s} : #{message}"
+  end
+
+  def self.log_color(color)
+
+    #
+    color = color.to_rgb
+
+    # Get each channel and prepare for loggers output
+    red   = color.r * 255
+    green = color.g * 255
+    blue  = color.b * 255
+
+    # Log the color and the hex of the color
+    log "████████ ".color(red, green, blue) + color.html
+
+  end
+
+end
--- lib/limitless_led/bridge.rb
+++ lib/limitless_led/bridge.rb
@@ -24,5 +24,71 @@ module LimitlessLed
       socket.send packet, 0
     end
 
+    def run(input)
+      command = input.bytes
+
+      case command.first
+      when 64
+        color command[1]
+
+      when 65
+        raise "Not implemented"
+
+      when 66
+        raise "Not implemented"
+
+      when 67
+        raise "Not implemented"
+
+      when 68
+        raise "Not implemented"
+
+      when 69
+        raise "Not implemented"
+
+      when 70
+        raise "Not implemented"
+
+      when 71
+        raise "Not implemented"
+
+      when 72
+        raise "Not implemented"
+
+      when 73
+        raise "Not implemented"
+
+      when 74
+        raise "Not implemented"
+
+      when 75
+        raise "Not implemented"
+
+      when 76
+        raise "Not implemented"
+
+      when 77
+        raise "Not implemented"
+
+      else
+        log "invalid command recieved: #{command.first}: #{command}"
+      end
+    end
+
+    private
+
+    def color(color)
+
+      # Turn second byte
+      color = color.to_f / 255.0 * 360.0
+
+      # Rotate 90 degrees and flip to match dial ui on device
+      hue = color - 90
+
+      # Return the color
+      Color::HSL.new( hue, 90, 50).to_rgb
+
+    end
+
   end
 end
